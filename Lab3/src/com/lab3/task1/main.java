3a848b8304fb5a6bd4644dea335bf76bdb663767
KkolyadkoO <artem.7ko52@gmail.com>
2025-03-18 22:15:45 +0300

    lab3 fix

--- Lab3/src/com/lab3/task1/main.java
+++ Lab3/src/com/lab3/task1/main.java
@@ -2,11 +2,12 @@ package com.lab3.task1;
 
 public class main {
     public static void main(String[] args) {
-        Level level = new LevelBuilder()
+        var levelB = new LevelBuilder()
                 .setDifficulty("Hard")
                 .addObstacle("Spikes")
-                .addObstacle("Lava")
-                .addMonster("Dragon")
+                .addObstacle("Lava");
+        levelB.reset();
+        var level = levelB.addMonster("Dragon")
                 .addMonster("Goblin")
                 .build();
 
